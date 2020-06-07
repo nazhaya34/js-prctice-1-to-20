aedcb056fa3d3d83ac3c50c6e221ceda24dcffe4
nazhaya34 <nazhaya34@gmail.com>
2020-06-07 13:35:08 +0500

    assignment

---
+++ app.js
@@ -0,0 +1,150 @@
+//chap 1
+// alert("Assalam O Alikum")
+// alert("Error! Please enter a valid code")
+// alert("Wellcome to Js land... \n Happy coding.")
+// alert("Wellcome to Js land") 
+// alert("Happy coding. \n Prevent this page for additional codeing")
+// console.log("Hello i can run js from my browser")
+//chap 2
+// var username = 'hiba';
+// var mynanme = username ;
+// alert(username)
+// let message = 'hello world';
+// alert(message);
+// var name = prompt('Name')
+// var father_name = prompt('Father name')
+// var qualification = prompt('Qualification')
+// var status = prompt('Status')
+// alert(name);
+// alert(father_name)
+// alert(qualification)
+// alert(status)
+// var str= 'PIZZA'
+// alert(str.slice(0,5)+'\n'+ str.slice(0,4)+'\n'+ str.slice(0,3)+'\n'+ str.slice(0,2) +'\n'+ str.slice(0,1))
+// var email = "abc@gmail.com"
+// var message = "My emaoil address is " + email
+// alert(message)
+// var book = "A smarter way to learn JavaScript"
+// alert("I am learning JS from " + book)
+// document.write('Yes I can run HTML thorugh JS')
+// var emoji='▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬'
+// alert(emoji)
+// //chap 3
+// var age = 22
+// alert('i m ' + age + 'year old')
+// alert('You visited this page ' + Math.random() + 'times' )
+// var birthyear = 1998;
+// document.write('My date of birth is ' + birthyear)
+// var visnam = 'John Doe'
+// var protitle = 'T-shirt'
+// var quantity = '5'
+// document.write(visnam  + ' ordered '+ quantity +" "+ protitle +  ' on XYZ Clothing store')
+//chap 4
+// var v = 3, h=3, j=4
+//illegal variable
+// var hiba naz
+// var =
+// var 'h'
+// var 7
+// var break
+// legal varibale
+//  var hiba_naz 
+//  var h2
+//  var @h
+//  var web
+ // var $3
+//  document.write('Rules for naming JS variables')
+// document.write('Variable names can only contain __number____, _letter_____,___strings___ and __char____.'+ '<br>')
+// document.write('Variables must begin with a num_, __letter, underscore____ or__ dollar___. For example $name, _name or name'+ '<br>')
+// document.write('Variable names are case __sensitive_______'+ '<br>')
+// document.write('Variable names should not be JS ___reserved keyword______'+ '<br>')
+// var x= Number(prompt())
+// var y = Number(prompt())
+
+//  document.write('Sum of ' + x + 'and ' + y + 'is' + x+y +'<br>' )
+// document.write('Subract of ' + x + 'and ' + y + 'is' + x-y + '<br>')
+// document.write('Multiplication of ' + x + 'and ' + y + 'is' + x*y + '<br>')
+// document.write('Divide of ' + x + 'and ' + y + 'is' + x/y+ '<br>')
+
+// var g 
+// document.write('value before initialization ' + g )
+//  g = 7
+//  document.write('\n After initialization ' + g)
+//  var h = g+1;
+//  document.write('\n'+ h)
+//  var i = h+7
+//  document.write('value after increament ' + i)
+//  document.write('value after decreament ' + i-1 )
+// document.write('reminder after divided by 3 ' + i%3)
+
+
+// var price= 600
+// document.write('Total cost of five movie ticket is '+ 5 *price)
+
+// var x = 4;
+// document.write( x + "*" + "1"+ '='  + x*1 + '<br>')
+// document.write(  x + "*" + "2"+ '='  + x*2+ '<br>')
+// document.write( x + "*" + "3"+ '='  + x*3+ '<br>')
+// document.write( x + "*" + "3"+ '='  + x*3+ '<br>')
+// document.write(x + "*" + "4"+ '='  + x*4+ '<br>')
+// document.write(x + "*" + "5"+ '='  + x*5+ '<br>')
+// document.write( x + "*" + "6"+ '='  + x*6+ '<br>')
+// document.write(x + "*" + "7"+ '='  + x*7+ '<br>')
+// document.write(x + "*" + "8"+ '='  + x*8+ '<br>')
+// document.write( x + "*" + "9"+ '='  + x*9+ '<br>')
+// document.write( x + "*" + "10"+ '='  + x*10+ '<br>')
+
+// var cel= 23;
+// var far = (cel * 9/5) + 32;
+// document.write("The far of "+ cel +" is " + far + '<br>' )
+//  var far = 97;
+//  var cel = (5-32) * 5/9
+//  document.write("The cel of "+ far +" is " + cel )
+
+// var price1 = 650
+// var price2 = 100
+// var q1 = 3
+// var q2 = 7
+// var schrges = 100
+// var t1= price1*q1
+// var t2 = price2*q2
+// var total = schrges+t1+t2
+
+// document.write("Price of item 1 is " + price1 +"<br>")
+// document.write("Quantity of item 1 is " + q1 +"<br>")
+// document.write("Price of item 2 is " + price2 +"<br>")
+// document.write("Quantity of item 2 is " + q2 +"<br>")
+// document.write("Shipping Charges is " + schrges +"<br>")
+// document.write("Total bill is " + total +"<br>")
+
+
+// var total = 300
+// var omarks = 150
+// var percentage = 150/300*(100)
+// document.write("total mark is  " + total +"<br>")
+// document.write("Marks obatianed is " + omarks +"<br>")
+// document.write('percentage is '+  percentage + '%')
+
+// var us = 10 , usp = us* 104.80 , sriyal= 25, sriyalp = sriyal* 28, total=sriyalp+usp
+// document.write("Total amount you have is " + total +"<br>")
+ 
+var x = 7, add= x+5, mul = x*10, div = x/2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
